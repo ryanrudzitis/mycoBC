a76142416a8c985e9cd74822ad79f622e8f1e806
ryanrudzitis <ryanrudzitis@icloud.com>
2021-12-27 21:14:36 -0800

    clicking button on card prints mushroom's binomial to console

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -6,25 +6,11 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 
+// function showMushroomInfo(props) {
+  
+// }
+
 const Record = (props) => (
-  // <tr>
-  //   <td>{props.record.name}</td>
-  //   <td>{props.record.binomial}</td>
-  //   <td>{props.record.edible}</td>
-  //   <td>{props.record.poisonous}</td>
-  //   <td>{props.record.availability}</td>
-  //   {/* <td>
-  //     <Link to={"/edit/" + props.record._id}>Edit</Link> |
-  //     <a
-  //       href="/"
-  //       onClick={() => {
-  //         props.deleteRecord(props.record._id);
-  //       }}
-  //     >
-  //       Delete
-  //     </a>
-  //   </td> */}
-  // </tr>
   <Card style={{ width: '18rem' }}>
     <Card.Img variant="top" src="holder.js/100px180" />
     <Card.Body>
@@ -33,7 +19,7 @@ const Record = (props) => (
         Some quick example text to build on the card title and make up the bulk of
         the card's content.
       </Card.Text>
-      <Button variant="primary" onClick={() => console.log(props.record.binomial)}>Learn more</Button>
+      <Button variant="primary" onClick={ () => showMushroomInfo(props)}>Learn more</Button>
     </Card.Body>
   </Card>
 );
@@ -73,8 +59,6 @@ export default class RecordList extends Component {
   // This method will map out the users on the table
   recordList() {
     return this.state.records.map((currentrecord) => {
-      console.log('hello');
-      console.log(this.state.records);
       return (
         <Record
           record={currentrecord}
@@ -85,37 +69,17 @@ export default class RecordList extends Component {
     });
   }
 
-  // This following section will display the table with the records of individuals.
+  // This following section will display a card for each database record
   render() {
     return (
       <div>
-        {/* <h3>Mushrooms!</h3>
-        <table className="table table-striped" style={{ marginTop: 20 }}>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Binomial</th>
-              <th>Edible</th>
-              <th>Poisonous</th>
-              <th>Availability</th>
-            </tr>
-          </thead>
-          <tbody>{this.recordList()}</tbody>
-        </table>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card> */}
         {this.recordList()}
       </div>
 
     );
   }
+}
+
+function showMushroomInfo(props) {
+  return console.log(props.record.binomial);
 }
